b3b155b077902360671b699ef3b47a726b743e23
YeduKrishnan A <kuttuzz17@gmail.com>
2023-03-11 10:32:30 +0530

    Form responsive fix and add Input text to image

--- src/Pages/Form.jsx
+++ src/Pages/Form.jsx
@@ -2,30 +2,11 @@ import image from '../assets/image.png'
 import memeData from './memeData';
 import React from 'react';
 
-/**
-   * Challenge: Update our state to save the meme-related
-   * data as an object called `meme`. It should have the
-   * following 3 properties:
-   * topText, bottomText, randomImage.
-   * 
-   * The 2 text states can default to empty strings for now,
-   * amd randomImage should default to "http://i.imgflip.com/1bij.jpg"
-   * 
-   * Next, create a new state variable called `allMemeImages`
-   * which will default to `memesData`, which we imported above
-   * 
-   * Lastly, update the `getMemeImage` function and the markup 
-   * to reflect our newly reformed state object and array in the
-   * correct way.
-   */
-
 
 export default function Form() {
-    // const [meme, setMeme] = React.useState('http://i.imgflip.com/1bij.jpg')
-
     const [meme, setMeme] = React.useState({
-        firstText: '',
-        secondText: '',
+        inputOne: '',
+        inputTwo: '',
         url: 'http://i.imgflip.com/1bij.jpg'
     })
 
@@ -33,7 +14,6 @@ export default function Form() {
 
     function randomMemeImages() {
         const memeImageArray = memeImage.data.memes;
-        // const memeImageArray = memeData.data.memes;
         const randomMemeImage = memeImageArray[Math.floor(Math.random() * memeImageArray.length)].url
         console.log(randomMemeImage)
         return randomMemeImage
@@ -57,19 +37,45 @@ export default function Form() {
 
     }
 
+    function handleClick(event) {
+        const { name, value } = event.target;
+        setMeme(prevMeme => {
+            return {
+                ...prevMeme,
+                [name]: value
+            }
+        })
+    }
+
     return (
         <main >
-            <p></p>
             <form action="#" className="form">
                 <div className="inputs">
-                    <input type="text" name="inputOne" className="inputOne" placeholder='Type Here Part One' />
-                    <input type="text" name="inputTwo" className="inputTwo" placeholder='Type Here Part Two' />
+
+                    <input type="text"
+                        name="inputOne"
+                        className="inputOne"
+                        placeholder="Type Here Part One"
+                        value={meme.inputOne}
+                        onChange={handleClick}
+                    />
+
+                    <input type="text"
+                        name="inputTwo"
+                        className="inputTwo"
+                        placeholder="Type Here Part Two"
+                        value={meme.inputTwo}
+                        onChange={handleClick}
+
+                    />
                 </div>
                 <button onClick={handleGetMeme} className="submitBtn">Get a meme image <img src={image} alt="image" /></button>
             </form>
 
             <div className='displayMemeImage'>
                 <img src={meme.url} alt="Getting Meme's" />
+                <h2 className="meme--text top">{meme.inputOne}</h2>
+                <h2 className="meme--text bottom">{meme.inputTwo}</h2>
             </div>
         </main>
     )
